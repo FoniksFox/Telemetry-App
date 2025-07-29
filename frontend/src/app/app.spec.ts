088cedb0c64feb75a0c196c855f8cbc0f9b1a398
Foniks <borisbeslimov@gmail.com>
2025-07-29 17:10:33 +0200

    style: Use 4 spaces for indentation (update some files that did not follow this rule)

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -3,21 +3,21 @@ import { App } from './app';
 
 describe('App', () => {
     beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        imports: [App],
-      }).compileComponents();
-    });
+      	await TestBed.configureTestingModule({
+        	imports: [App],
+      	}).compileComponents();
+	});
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(App);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(App);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-    it('should render title', () => {
-      const fixture = TestBed.createComponent(App);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
-    });
+	it('should render title', () => {
+		const fixture = TestBed.createComponent(App);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+	});
 });
