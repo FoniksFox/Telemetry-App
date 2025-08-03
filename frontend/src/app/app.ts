a0315a7ce776f4f201482deedb6d3ec0b5c0df5e
Foniks <borisbeslimov@gmail.com>
2025-08-03 16:28:58 +0200

    feat(frontend): Basic UI top-layout structure

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.html',
-    styleUrl: './app.scss'
+    imports: [RouterOutlet, Header, Sidebar],
+    template: `
+    <main class="flex-between flex-column">
+        <app-header></app-header>
+        <div class="content flex-between flex-row">
+            <app-sidebar></app-sidebar>
+            <router-outlet></router-outlet>
+        </div>
+    </main>
+    `,
+    styles: `
+    main {
+        width: 100vw;
+        height: 100vh;
+    }
+    .content {
+        flex-grow: 1;
+        width: 100%;
+    }
+    `
 })
 export class App {
     protected readonly title = signal('frontend');
